6a69b47d78e0041dfd9db4f5e76187a1d2c26f98
KevinSu <kevin86677372@yahoo.com.tw>
2021-11-03 19:04:09 -0500

    Init

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
